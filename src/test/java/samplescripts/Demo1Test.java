b1f2bb105ac62aa4196d1441636ff7cbff35e839
pallavi <pallavi@LAPTOP-GQOS4IUB>
2023-07-07 11:45:58 +0530

    1st commit

---
+++ src/test/java/samplescripts/Demo1Test.java
@@ -0,0 +1,14 @@
+package samplescripts;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Demo1Test {
+
+	@Test
+	public void t1()
+	{
+	System.out.println("hello");
+	}
+}
